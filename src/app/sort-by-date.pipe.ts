cd4125d0a54bda328d1be648e55db70a7fd71acc
GuilhemMagaud <guilhem.magaud@orange.fr>
2023-05-18 22:29:52 +0200

    obsevable work

--- src/app/sort-by-date.pipe.ts
+++ src/app/sort-by-date.pipe.ts
@@ -7,7 +7,7 @@ import { Product } from './models/product.model';
 export class SortByDatePipe implements PipeTransform {
 
   transform(product: Product[]): Product[]{
-    return product.sort((a,b) => {return b.date.getTime() * a.date.getTime()}); 
+    return product.sort((a,b) => {const dateA: Date = new Date(a.date); const dateB: Date = new Date(b.date); return dateB.getTime() * dateA.getTime()});
   }
 
 }
